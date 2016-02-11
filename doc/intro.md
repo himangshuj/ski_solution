579b83ac7bfa9838c53d270826bd437042072a75
himangshu <himangshu@gozoomo.com>
2016-02-12 00:28:44 +0530

    initial commit
    Signed-off-by: himangshu <himangshu@gozoomo.com>

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ski_solution
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ski_solution/core_test.clj
@@ -0,0 +1,7 @@
+(ns ski-solution.core-test
+  (:require [clojure.test :refer :all]
+            [ski-solution.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
