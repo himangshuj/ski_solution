93e39e873ffc1d9717a43eb4e28852b56e4d755c
himangshu <himangshu@gozoomo.com>
2016-02-12 12:02:03 +0530

    Code working for the smaller input file
    Signed-off-by: himangshu <himangshu@gozoomo.com>

--- src/ski_solution/core.clj
+++ src/ski_solution/core.clj
@@ -18,7 +18,7 @@
        :data  (doall (map
                        (fn [line row]
                          (map
-                           #(hash-map :val  (Integer/parseInt %) :row row :column %2 :hops 0 :start-val (Integer/parseInt %)
+                           #(hash-map :val  (Integer/parseInt %) :row row :column %2 :hops 1 :start-val (Integer/parseInt %)
                                       :propagated false)
                            (string/split line #"\s")
                            (iterate inc 0)))
@@ -31,51 +31,62 @@
         (or
           (> (:hops node-src) (:hops node-sink))
           (and (= (:hops node-src) (:hops node-sink)) (> (:start-val node-src) (:start-val node-sink)))))
-    (merge node-sink {:start-val (node-src :start-val) :hops (+ (:hops node-src) 1 ) })))
+    (merge node-sink {:start-val (node-src :start-val) :hops (+ (:hops node-src) 1 ) :propagated false })))
 
 (defn- propagate-node [node graph ]
   (let [ {row :row column :column} (select-keys node [:row :column])
         north (graph {:row (- row 1) :column column})
         north# (propagate-edge node north)
-        north-west (graph {:row (- row 1) :column (- column 1) })
-        north-west# (propagate-edge node north-west)
-        north-east (graph {:row (- row 1) :column (+ column 1) })
-        north-east# (propagate-edge node north-east)
-        south-east (graph {:row (+ row 1) :column (+ column 1) })
-        south-east# (propagate-edge node south-east)
         south (graph {:row (+ row 1) :column column })
         south# (propagate-edge node south)
-        south-west (graph {:row (+ row 1) :column (- column 1) })
-        south-west# (propagate-edge node south-west)
-        new-nodes [north# north-east# north-west# south# south-west# south-east#]
+        east (graph {:row row :column (+ column 1) })
+        east# (propagate-edge node east)
+        west (graph {:row row :column (- column 1) })
+        west# (propagate-edge node west)
+        new-nodes [north#  south# east# west#]
         new-nodes# (remove nil? new-nodes)
         graph# (reduce #(merge %1  {(select-keys %2 [:row :column]) %2} ) graph new-nodes#)
-        graph# (update-in graph# (select-keys node [:row :column]  )
-                          #(merge % {:propagated true}))
-        ]
-    graph#
-    ))
+        node# (merge node {:propagated true})
+        graph# (merge graph# {(select-keys node [:row :column]) node#})]
+    graph#))
+
 (def node-comparator
   (fn [node1 node2]
-    (or (:propagated node2)
+    (or
         (> (:hops node1) (:hops node2))
         (and (= (:hops node1) (:hops node2))
-             (> (:val node1) (:val node2)))
+             (> (- (:start-val node1) (:val node1)) (- (:start-val node2) (:val node2))))
         (and (= (:hops node1) (:hops node2))
-             (= (:val node1) (:val node2))
-             (> (:start-val node1) (:start-val node2)))
+             (= (- (:start-val node1) (:val node1)) (- (:start-val node2) (:val node2)))
+             (> (:val node1) (:val node2)))
         )
     ))
 
+;; we first initialize the graph with each node having the elevation stored as val
+;; hops as 1 and starting elevation as start val which is the same as elevation of the node
+;; now we implement modified djikstra of shortest path path1 is shorter than path2
+;; if the number of hopes to reach destination node is more or drop is more than through path2
+;; and final tiebreaker the elevation at path1 is more than elevation at path2
 
 (defn -main []
   (let [input-data (read-file "input1.txt")
         input-data# (-> input-data :data flatten)
         input-data# (reduce #(merge %1 {{:row (:row %2) :column (:column %2)} %2 } ) {} input-data#)
-        sorted-input-data (sort node-comparator (vals input-data#))
-        edge-to-propagate (input-data# (select-keys (first sorted-input-data) [:row :column]) )
-        ]
-    (println (vals input-data#))
-    (println edge-to-propagate)
-    )
-  (println "aaa"))
+        sorted-input-data (sort node-comparator (vals input-data#))]
+
+    (loop [edge-key (select-keys (first sorted-input-data) [:row :column])
+           graph input-data#
+           iteration 1]
+      (let [
+            edge-to-propagate (graph edge-key)
+            new-graph (propagate-node edge-to-propagate graph)
+            unprocessed-graph (remove #(-> %  :propagated) (vals new-graph))
+            sorted-unprocessed (sort node-comparator unprocessed-graph )
+            new-edge-to-propagate (first sorted-unprocessed)]
+        (if new-edge-to-propagate
+          (recur (select-keys new-edge-to-propagate [:row :column])
+                 new-graph
+                 (+ 1 iteration))
+          (do
+            (println
+              (first (sort node-comparator (vals new-graph)) ))))))))
