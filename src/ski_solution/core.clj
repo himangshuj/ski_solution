312db1787c93b0c45aee525ffe6c0e8529a9cd98
himangshu <himangshu@gozoomo.com>
2016-02-12 19:24:39 +0530

    Fixing comparator
    Signed-off-by: himangshu <himangshu@gozoomo.com>

--- src/ski_solution/core.clj
+++ src/ski_solution/core.clj
@@ -4,34 +4,11 @@
   (:import java.util.PriorityQueue)
   (:gen-class))
 
-(defn SkiGrid->map [skigrid-obj]
-  {:val (.val skigrid-obj)
-   :start-val (.start-val skigrid-obj)
-   :row (.row skigrid-obj)
-   :column (.column skigrid-obj)
-   :hops (.hops skigrid-obj)
-   :propagated (.propagated skigrid-obj)})
 
-(deftype SkiGrid [val start-val row column hops propagated]
-  Object
-  (equals [a b] (and (= (.row a) (.row b) )
-                     (= (.column a) (.column b))))
-  (hashCode [a] (* (.column a) (.row a)))
-  (toString [a]  (str (SkiGrid->map a)) ))
+(defrecord SkiGrid [val start-val row column hops propagated])
 
-(defn- get-ski-cordinates [ski-grid]
-  {:row (.row ski-grid) :column (.column ski-grid)})
-
-
-(defn map->SkiGrid [map-obj]
-  (SkiGrid. (:val map-obj)
-            (:start-val map-obj)
-            (:row map-obj)
-            (:column map-obj)
-            (:hops map-obj)
-            (:propagated map-obj)))
-
-
+(defn- get-ski-cordinates [node]
+  (select-keys node [:row :column]))
 
 
 
@@ -52,17 +29,15 @@
                            (iterate inc 0)))
                        (rest lines) (iterate inc 0)))})))
 
-(defn- propagate-edge [node-src-in node-sink-in]
-(if (and node-src-in node-sink-in)
-  (let [node-src (SkiGrid->map node-src-in )
-        node-sink (SkiGrid->map node-sink-in )]
-    (if (and
-          (and node-src node-sink )
-          (> (:val node-src) (:val node-sink))
-          (or
-            (> (:hops node-src) (:hops node-sink))
-            (and (= (:hops node-src) (:hops node-sink)) (> (:start-val node-src) (:start-val node-sink)))))
-      (map->SkiGrid  (merge node-sink {:start-val (node-src :start-val) :hops (+ (:hops node-src) 1 ) :propagated false }))))))
+(defn- propagate-edge [node-src node-sink]
+  (if  (and
+         (and node-src node-sink )
+         (> (:val node-src) (:val node-sink))
+         (or
+           (> (:hops node-src) (:hops node-sink))
+           (and (= (:hops node-src) (:hops node-sink)) (> (:start-val node-src) (:start-val node-sink)))
+           )) (merge node-sink {:start-val (node-src :start-val) :hops (+ (:hops node-src) 1 ) :propagated false })))
+
 
 
 
@@ -78,25 +53,20 @@
         west# (propagate-edge node west)
         neighbor-nodes [north#  south# east# west#]
         neighbor-nodes# (remove nil? neighbor-nodes)
-        nodes-come-to-play (filter #(and (-> % second  :propagated not))
-                                   [ [north north#] [east east#] [west west#] [south south#]]) ;;these were earlier marked as calculated need to recalculate
-        nodes-come-to-play# (map second nodes-come-to-play)
-        nodes-come-to-play# (remove nil? nodes-come-to-play#)
-        graph# (reduce #(merge %1  {(get-ski-cordinates %2 ) %2} ) graph neighbor-nodes#)
-        node# (SkiGrid->map node)
-        node# (merge node# {:propagated true})
-        graph# (assoc graph# (select-keys node# [:row :Column]) (map->SkiGrid node#))]
-    {:graph graph# :nodes-modified nodes-come-to-play# :nodes-modified-org neighbor-nodes# }  ))
+        _ (println neighbor-nodes#)
+        graph# (reduce #(assoc %1  (get-ski-cordinates %2 ) %2 ) graph neighbor-nodes#)
+        node# (merge node {:propagated true})
+        graph# (assoc graph# (get-ski-cordinates node# ) (map->SkiGrid node#))]
+    {:graph graph# :nodes-modified neighbor-nodes#}  ))
 
 (def node-comparator
   (fn [node1 node2]
-    (or
-        (> (.hops node1) (.hops node2))
-        (and (= (.hops node1) (.hops node2))
-             (> (- (.start-val node1) (.val node1)) (- (.start-val node2) (.val node2))))
-        (and (= (.hops node1) (.hops node2))
-             (= (- (.start-val node1) (.val node1)) (- (.start-val node2) (.val node2)))
-             (> (.val node1) (.val node2))))))
+    (let [heuristic1 (+ (.hops node1) (.val node1))
+          heuristic2 (+ (.hops node2) (.val node2))]
+      (or
+        (> heuristic1 heuristic2)
+        (and (= heuristic1 heuristic2)
+             (> (- (.start-val node1) (.val node1)) (- (.start-val node2) (.val node2))))))))
 
 
 
@@ -107,7 +77,7 @@
 ;; and final tiebreaker the elevation at path1 is more than elevation at path2
 
 (defn -main []
-  (let [input-data (read-file "map.txt")
+  (let [input-data (read-file "input1.txt")
         input-data# (-> input-data :data flatten)
         input-data# (reduce #(merge %1 {{:row (.row %2) :column (.column %2)} %2 } ) {} input-data#)
         _ (println "made data into map")
@@ -122,18 +92,20 @@
       (let [ ;;removes one element from the priority queue
             edge-to-propagate (graph edge-key)
             {new-graph :graph
-             nodes-modified :nodes-modified
-             nodes-modified-org :nodes-modified-org}  (propagate-node edge-to-propagate graph)
+             nodes-modified :nodes-modified} (propagate-node edge-to-propagate graph)
             _ (println "\n------------------------------\n")
             _ (println "iteration " iteration)
             _ (println edge-to-propagate)
-            _ (println "\n------------------------------\n")
+            _ (println "\n------------------------------ Before \n" priority-queue )
             _ (doto priority-queue
-                (.removeAll nodes-modified-org)
+                (.removeAll nodes-modified)
                 (.addAll nodes-modified))
-            _ (doto result-pq
-                (.remove edge-to-propagate)
-                (.add (new-graph edge-key)))
+            _ (if (empty? nodes-modified)
+                (doto result-pq
+                  (.remove edge-to-propagate)
+                  (.add (new-graph edge-key))))
+            _ (println "\n------------------------------ After \n" priority-queue )
+
             ;_ (println "pq size " (count priority-queue))
             _ (println "result pq top " (. result-pq (peek)))
             _ (println "\n------------------------------\n")]
--- test/ski_solution/core_test.clj
+++ test/ski_solution/core_test.clj
@@ -1,26 +1,29 @@
 (ns ski-solution.core-test
   (:require [clojure.test :refer :all]
-            [ski-solution.core :refer :all]))
+            [ski-solution.core :refer :all])
+  (:import [java.util PriorityQueue Collections]))
 
-
+(def vals (map
+            map->SkiGrid
+            [{:start-val 4, :val 4, :hops 0, :column 0, :propagated false, :row 0}
+             {:start-val 9, :val 9, :hops 1, :column 2, :propagated false, :row 1}
+             {:start-val 3, :val 3, :hops 2, :column 1, :propagated false, :row 2}
+             {:start-val 5, :val 5, :hops 0, :column 3, :propagated false, :row 2}
+             {:start-val 5, :val 5, :hops 0, :column 1, :propagated false, :row 1}
+             {:start-val 6, :val 6, :hops 0, :column 0, :propagated false, :row 2}
+             {:start-val 4, :val 4, :hops 0, :column 0, :propagated false, :row 3}
+             {:start-val 8, :val 6, :hops 0, :column 3, :propagated false, :row 3}]))
 
 (deftest comparision-test
   (testing "comparision tests"
-    (let [vals [{:start-val 4, :val 4, :hops 0, :column 0, :propagated false, :row 0}
-                {:start-val 9, :val 9, :hops 1, :column 2, :propagated false, :row 1}
-                {:start-val 3, :val 3, :hops 2, :column 1, :propagated false, :row 2}
-                {:start-val 5, :val 5, :hops 0, :column 3, :propagated false, :row 2}
-                {:start-val 5, :val 5, :hops 0, :column 1, :propagated false, :row 1}
-                {:start-val 6, :val 6, :hops 0, :column 0, :propagated false, :row 2}
-                {:start-val 4, :val 4, :hops 0, :column 0, :propagated false, :row 3}
-                {:start-val 8, :val 6, :hops 0, :column 3, :propagated false, :row 3} ]
-          vals-sorted (sort node-comparator vals) ]
+    (let [pq          (PriorityQueue. node-comparator)
+          _           (. pq (addAll vals))
+          vals-sorted (sort node-comparator vals)]
       (are [x y] (= x y)
-                 (first vals-sorted)
-                 {:start-val 3, :val 3, :hops 2, :column 1, :propagated false, :row 2}
-                 (second vals-sorted)
-                 {:start-val 9, :val 9, :hops 1, :column 2, :propagated false, :row 1}
-                 (nth vals-sorted 2)
-                 {:start-val 8, :val 6, :hops 0, :column 3, :propagated false, :row 3}
-                 (nth vals-sorted 3)
-                 {:start-val 6, :val 6, :hops 0, :column 0, :propagated false, :row 2}))))
+                 (first vals-sorted) (map->SkiGrid {:start-val 9, :val 9, :hops 1, :column 2, :propagated false, :row 1})
+                 (first vals-sorted) (. pq (poll))
+                 (second vals-sorted) (. pq (poll))
+                 (. pq (peek)) (do
+                                 (doto pq
+                                   (.add (map->SkiGrid {:start-val 3, :val 3, :hops 2, :column 1, :propagated false, :row 2})))
+                                 (. pq (peek))) ))))
