cc63c972a1e3272693ca60fbbcd735133c3865d1
himangshu <himangshu@gozoomo.com>
2016-02-13 13:00:21 +0530

    Used a correct implementation of fibonacci heap
    Signed-off-by: himangshu <himangshu@gozoomo.com>

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ski-solution.core
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.jgrapht/jgrapht-core "0.9.1"]])
+  :java-source-paths ["java-src"]
+  :dependencies [[org.clojure/clojure "1.8.0"]])
--- test/ski_solution/core_test.clj
+++ test/ski_solution/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [ski-solution.core :refer :all])
   (:import [java.util PriorityQueue]
-           [org.jgrapht.util FibonacciHeapNode FibonacciHeap])
+           [ski_solution.util FibonacciHeap])
   (:refer-clojure :exclude [peek]))
 
 (def vals (map
@@ -22,7 +22,7 @@
           _           (. pq (addAll vals))
           vals-sorted (sort node-comparator vals)]
       (are [x y] (= (select-keys x [:val :hops])
-                    (select-keys  y [:val :hops]))
+                    (select-keys y [:val :hops]))
                  (first vals-sorted) (map->SkiGrid {:start-val 9, :val 9, :hops 1, :column 2, :propagated false, :row 1})
                  (first vals-sorted) (. pq (poll))
                  (second vals-sorted) (. pq (poll))
@@ -51,7 +51,7 @@
           _           (. pq (addAll queue-list))
           queue-list# (sort node-comparator queue-list)]
       (are [x y] (= (select-keys x [:val :hops])
-                    (select-keys  y [:val :hops]))
+                    (select-keys y [:val :hops]))
                  (nth queue-list# 0) (. pq (poll))
                  (nth queue-list# 1) (. pq (poll))
                  (nth queue-list# 2) (. pq (poll))
@@ -83,26 +83,71 @@
                        (map->SkiGrid {:val 4, :start-val 4, :row 3, :column 1, :hops 1, :propagated false}),
                        (map->SkiGrid {:val 3, :start-val 9, :row 1, :column 3, :hops 2, :propagated false})]
           queue-list# (sort node-comparator queue-list)
-          fb (FibonacciHeap. )
-          _ (doseq [node queue-list]
-              (let [node# (FibonacciHeapNode. node)]
-                (. fb (insert node# (get-fibonacci-key node)))))]
+          fb          (FibonacciHeap.)
+          _           (doseq [node queue-list]
+                        (let []
+                          (. fb (enqueue node (get-fibonacci-key node)))))]
+      (are [x y] (= (select-keys x [:val :hops :start-val])
+                    (select-keys (. y (getValue) )  [:val :hops :start-val]))
+                 (nth queue-list# 0) (. fb (dequeueMin))
+                 (nth queue-list# 1) (. fb (dequeueMin))
+                 (nth queue-list# 2) (. fb (dequeueMin))
+                 (nth queue-list# 3) (. fb (dequeueMin))
+                 (nth queue-list# 4) (. fb (dequeueMin))
+                 (nth queue-list# 5) (. fb (dequeueMin))
+                 (nth queue-list# 6) (. fb (dequeueMin))
+                 (nth queue-list# 7) (. fb (dequeueMin))
+                 (nth queue-list# 8) (. fb (dequeueMin))
+                 (nth queue-list# 9) (. fb (dequeueMin))
+                 (nth queue-list# 10) (. fb (dequeueMin))
+                 (nth queue-list# 11) (. fb (dequeueMin))
+                 (nth queue-list# 12) (. fb (dequeueMin)))))
 
-      (are [x y] (= (select-keys x [:val  :hops])
-                    (select-keys (. y (getData))  [:val  :hops]))
-                 (nth queue-list# 0) (. fb (removeMin))
-                 (nth queue-list# 1) (. fb (removeMin))
-                 (nth queue-list# 3) (. fb (removeMin))
-                 (nth queue-list# 3) (. fb (removeMin))
-                 (nth queue-list# 4) (. fb (removeMin))
-                 (nth queue-list# 5) (. fb (removeMin))
-                 (nth queue-list# 6) (. fb (removeMin))
-                 (nth queue-list# 7) (. fb (removeMin))
-                 (nth queue-list# 8) (. fb (removeMin))
-                 (nth queue-list# 9) (. fb (removeMin))
-                 (nth queue-list# 10) (. fb (removeMin))
-                 (nth queue-list# 11) (. fb (removeMin))
-                 (nth queue-list# 12) (. fb (removeMin))))))
+  (testing "fibonacci decrease key test"
+    (let [queue-list  [(map->SkiGrid {:val 7, :start-val 7, :row 0, :column 2, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 6, :start-val 6, :row 3, :column 3, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 8, :start-val 8, :row 0, :column 1, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 5, :start-val 5, :row 2, :column 3, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 4, :start-val 4, :row 3, :column 0, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 6, :start-val 6, :row 2, :column 0, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 5, :start-val 5, :row 1, :column 1, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 4, :start-val 4, :row 0, :column 0, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 1, :start-val 1, :row 3, :column 2, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 2, :start-val 2, :row 1, :column 0, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 3, :start-val 3, :row 0, :column 3, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 3, :start-val 3, :row 2, :column 1, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 2, :start-val 2, :row 2, :column 2, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 4, :start-val 4, :row 3, :column 1, :hops 1, :propagated false}),
+                       (map->SkiGrid {:val 3, :start-val 3, :row 1, :column 3, :hops 1, :propagated false})]
+          queue-list# (sort node-comparator queue-list)
+          fb          (FibonacciHeap.)
+
+
+          entries     (reduce #(assoc %1 (get-ski-cordinates %2)
+                                         (. fb (enqueue %2 (get-fibonacci-key %2)))) {} queue-list)
+          _           (. fb (decreaseKey (entries (->SkiGridCordinate 0 2))
+                                         (get-fibonacci-key {:val 7, :start-val 9, :row 0, :column 2, :hops 2, :propagated false})))
+
+
+          _           (. fb (decreaseKey (entries (->SkiGridCordinate 2 2))
+                                         (get-fibonacci-key {:val 2, :start-val 9, :row 2, :column 2, :hops 2, :propagated false})))
+
+
+          _           (. fb (decreaseKey (entries (->SkiGridCordinate 1 3))
+                                         (get-fibonacci-key {:val 3, :start-val 9, :row 1, :column 3, :hops 2, :propagated false})))
+
+
+          _           (. fb (decreaseKey (entries (->SkiGridCordinate 1 1))
+                                         (get-fibonacci-key {:val 5, :start-val 9, :row 1, :column 1, :hops 2, :propagated false})))
+
+          _           (println (.. fb (dequeueMin) (getValue)))
+          _           (println (.. fb (dequeueMin) (getValue)))
+          _           (println (.. fb (dequeueMin) (getValue)))
+
+          ]
+
+      (is (= 1 1))
+      )))
 
 
 
